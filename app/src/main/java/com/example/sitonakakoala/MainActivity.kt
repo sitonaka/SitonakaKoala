da71f2a9b6f7685bb805fef340e0faec1c4e4f7a
sitonaka <csitonaka@gmail.com>
2024-08-14 16:04:45 +0900

    Step 4 AlertDialog.

--- app/src/main/java/com/example/sitonakakoala/MainActivity.kt
+++ app/src/main/java/com/example/sitonakakoala/MainActivity.kt
@@ -7,13 +7,52 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
 import com.example.sitonakakoala.screen.HomeScreen
+import com.example.sitonakakoala.screen.MainAlertDialog
 import com.example.sitonakakoala.ui.theme.SitonakaKoalaTheme
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlin.coroutines.Continuation
+import kotlin.coroutines.resume
+import kotlin.coroutines.suspendCoroutine
 
 class MainActivity : ComponentActivity() {
+
+    companion object {
+        private lateinit var Instance: MainActivity
+        suspend fun dialog(
+            text: String,
+            confirm: String = "OK",
+            dismiss: String? = null,
+            title: String? = null
+        ): Boolean {
+            return Instance.showAlertDialog(confirm, dismiss, title, text)
+        }
+    }
+
+    private val confirmFlow: MutableStateFlow<String> = MutableStateFlow("")
+    private val dismissFlow: MutableStateFlow<String?> = MutableStateFlow(null)
+    private val titleFlow: MutableStateFlow<String?> = MutableStateFlow(null)
+    private val textFlow: MutableStateFlow<String> = MutableStateFlow("")
+
+    private lateinit var alertContinuation: Continuation<Boolean>
+    private suspend fun showAlertDialog(
+        confirm: String,
+        dismiss: String?,
+        title: String?,
+        text: String
+    ): Boolean = suspendCoroutine {
+        alertContinuation = it
+        confirmFlow.value = confirm
+        dismissFlow.value = dismiss
+        titleFlow.value = title
+        textFlow.value = text
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        Instance = this
         enableEdgeToEdge()
         setContent {
             SitonakaKoalaTheme {
@@ -21,6 +60,21 @@ class MainActivity : ComponentActivity() {
                     HomeScreen(
                         modifier = Modifier.padding(innerPadding)
                     )
+                }
+                val confirm = confirmFlow.collectAsState()
+                val dismiss = dismissFlow.collectAsState()
+                val title = titleFlow.collectAsState()
+                val text = textFlow.collectAsState()
+                if (text.value.isNotEmpty()) {
+                    MainAlertDialog(
+                        confirm = confirm.value,
+                        dismiss = dismiss.value,
+                        title = title.value,
+                        text = text.value
+                    ) {
+                        alertContinuation.resume(it)
+                        textFlow.value = ""
+                    }
                 }
             }
         }
--- app/src/main/java/com/example/sitonakakoala/screen/HomeScreen.kt
+++ app/src/main/java/com/example/sitonakakoala/screen/HomeScreen.kt
@@ -4,17 +4,23 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.sitonakakoala.MainActivity
 import com.example.sitonakakoala.ui.theme.SitonakaKoalaTheme
+import kotlinx.coroutines.launch
 
 
 @Composable
 fun HomeScreen(modifier: Modifier = Modifier) {
+    val scope = rememberCoroutineScope()
     Column(modifier = modifier) {
         Text(text = "Hello HomeScreen!")
         Button(onClick = {
-            // Under construction
+            scope.launch {
+                MainActivity.dialog("Message from Home.")
+            }
         }) {
             Text(text = "Get Location")
         }
