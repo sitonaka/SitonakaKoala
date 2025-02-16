d39b3ba52e17b665209e666c3b6acf46d83fd4dc
sitonaka <csitonaka@gmail.com>
2025-02-16 09:50:07 +0900

    Step 20 Room Singleton.

--- app/src/main/java/com/example/sitonakakoala/screen/CommScreen.kt
+++ app/src/main/java/com/example/sitonakakoala/screen/CommScreen.kt
@@ -1,17 +1,31 @@
 package com.example.sitonakakoala.screen
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Button
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.sitonakakoala.MainActivity
+import com.example.sitonakakoala.database.MyDatabase
 import com.example.sitonakakoala.server.GHUsers
 import com.example.sitonakakoala.ui.theme.SitonakaKoalaTheme
 import io.ktor.client.HttpClient
@@ -27,8 +41,9 @@ import kotlinx.serialization.json.Json
 @Composable
 fun CommScreen(modifier: Modifier = Modifier) {
     val scope = rememberCoroutineScope()
+    val dao = MyDatabase.getDatabase(LocalContext.current).ghDao()
+    val users = dao.getAllUsersFlow().collectAsState(initial = listOf())
     var login by rememberSaveable { mutableStateOf("sitonaka") }
-    var responseBody by rememberSaveable { mutableStateOf("") }
     Column(modifier = modifier) {
         Text(text = "Login:  $login")
         Button(onClick = {
@@ -47,7 +62,7 @@ fun CommScreen(modifier: Modifier = Modifier) {
                         install(Logging)
                     }.use { client ->
                         val ghUsers: GHUsers = client.get("https://api.github.com/users/$login").body()
-                        responseBody = "${ghUsers.login} ${ghUsers.publicRepos} ${ghUsers.updatedAt}"
+                        dao.insertUsers(ghUsers)
                     }
                 }.onFailure {
                     MainActivity.dialog(it.localizedMessage ?: "error")
@@ -56,7 +71,34 @@ fun CommScreen(modifier: Modifier = Modifier) {
         }) {
             Text(text = "USERS API")
         }
-        Text(text = responseBody)
+        HorizontalDivider()
+        UsersItems(list = users.value)
+    }
+}
+
+@Composable
+fun UsersItems(list: List<GHUsers>) {
+    LazyColumn {
+        items(list) {
+            UsersItem(ghUsers = it)
+            HorizontalDivider()
+        }
+    }
+}
+
+@Composable
+fun UsersItem(ghUsers: GHUsers) {
+    Column {
+        Text(text = ghUsers.login, style = MaterialTheme.typography.titleLarge)
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.End,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Text(text = "updateAt", style = MaterialTheme.typography.bodySmall)
+            Spacer(modifier = Modifier.width(8.dp))
+            Text(text = ghUsers.updatedAt, style = MaterialTheme.typography.bodySmall)
+        }
     }
 }
 
@@ -65,5 +107,46 @@ fun CommScreen(modifier: Modifier = Modifier) {
 fun CommScreenPreview() {
     SitonakaKoalaTheme {
         CommScreen()
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun UsersItemsPreview() {
+    SitonakaKoalaTheme {
+        UsersItems(
+            listOf(
+                GHUsers(
+                    id = 0,
+                    login = "login name",
+                    publicRepos = 99,
+                    reposUrl = "http://google.com",
+                    updatedAt = "2000/12/31"
+                ),
+                GHUsers(
+                    id = 1,
+                    login = "login name 2",
+                    publicRepos = 999,
+                    reposUrl = "https://google.com",
+                    updatedAt = "2001/12/31"
+                )
+            )
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun UsersItemPreview() {
+    SitonakaKoalaTheme {
+        UsersItem(
+            GHUsers(
+                id = 0,
+                login = "login name",
+                publicRepos = 99,
+                reposUrl = "http://google.com",
+                updatedAt = "2000/12/31"
+            )
+        )
     }
 }
